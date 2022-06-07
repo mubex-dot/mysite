341fa2c2ddef67e876ac0b39aca2e2e5240b0aef
mubex-dot <63114194+mubex-dot@users.noreply.github.com>
2022-06-07 21:47:38 +0100

    Add files via upload

---
+++ Simple calculator/index.js
@@ -0,0 +1,23 @@
+let num1 =  10
+let num2 = 5
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+let sumEl = document.getElementById("sum-el")
+
+function add() {
+    let result = num1 + num2
+    sumEl.textContent = "sum: " + result
+}
+function subtract() {
+    let result = num1 - num2
+    sumEl.textContent = "sum: " + result
+}
+function divide() {
+    let result = num1 / num2
+    sumEl.textContent = "sum: " + result
+}
+function multiply() {
+    let result = num1 * num2
+    sumEl.textContent = "sum: " + result
+}
